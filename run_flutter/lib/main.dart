4a6a1d0dea57bcc61c8ac3551b852a85b375d5b6
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-06-23 00:39:07 +0900

    TransForm, Icon 활용하여 Card 만들기

--- run_flutter/lib/main.dart
+++ run_flutter/lib/main.dart
@@ -1,29 +1,28 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:run_flutter/widgets/button.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-      backgroundColor: Color(0xFF181818),
+        home: Scaffold(
+      backgroundColor: const Color(0xFF181818),
       body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 20
-          ),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 60,
               ),
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
@@ -48,50 +47,120 @@ class App extends StatelessWidget {
                   )
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 80,
               ),
-              Text('Total Balance',
+              Text(
+                'Total Balance',
                 style: TextStyle(
-                  color: Colors.white.withOpacity(0.8), 
-                  fontSize: 20),),
-              SizedBox(
+                    color: Colors.white.withOpacity(0.8), 
+                    fontSize: 20,
+                ),
+              ),
+              const SizedBox(
                 height: 10,
               ),
-              Text(
+              const Text(
                 '\$5,000,000',
                 style: TextStyle(
-                  color: Colors.white, 
-                  fontSize: 42,
-                  fontWeight: FontWeight.w600),
+                    color: Colors.white,
+                    fontSize: 42,
+                    fontWeight: FontWeight.w600),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 30,
               ),
-              Row(
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(45),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 20, 
-                        horizontal: 50,),
-                      child: Text('Transfer', 
-                        style: TextStyle(
-                          fontSize: 20,
+                  Button(text: 'Transfer', bgColor: Colors.amber, textColor: Colors.black,),
+                  Button(text: 'Request', bgColor: Color(0xFF1F2123), textColor: Colors.white,),
+                ],
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                const Text(
+                    'Wallets', 
+                    style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+                Text('View All', style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 18),),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF1F2123),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Euro',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w500),),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            children: [
+                              Text('6.428', 
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ), 
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text('EUR', 
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Transform.scale(
+                        scale: 2.2,
+                        child: Transform.translate(
+                          offset: const Offset(-8, 10),
+                          child: const Icon(Icons.euro_rounded, 
+                            color: Colors.white,
+                            size: 88,
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
-              )
+                    ],
+                  ),
+                ),
+              ),
             ],
           )
         ),
-      )
-    );
+    ));
   }
 }
